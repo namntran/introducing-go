93aa98d0538a434bd3d6a20e68e057d8fd3a6f9c
namntran <namtran78@gmail.com>
2018-08-06 07:50:27 +1000

    amend comments

--- ch5/ex12/main.go
+++ ch5/ex12/main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	arr := [5]float64{1, 2, 3, 4, 5} //create a slice with an underlying float64 array of length 5
-	x := arr[3:4]                    //access the 4th elememt of an array. create a slice using the [low : high] expression - index
+	x := arr[3:4]                    //access the 4th element of an array. create a slice using the [low : high] expression - index
 	fmt.Println(x)
 }
