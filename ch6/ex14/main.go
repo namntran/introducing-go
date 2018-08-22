57fa7babec1e2e3fea2eeb7b951cf81aeb453b29
namntran <namtran78@gmail.com>
2018-08-23 08:35:57 +1000

    add exercise using sum function that takes a slice of numbers and adds them together

---
+++ ch6/ex14/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func sum(input ...int) int { // sum is a function that takes a slice of numbers and adds them together
+	sum := 0
+
+	for _, i := range input {
+		sum += i
+	}
+	fmt.Println(sum)
+	return sum
+}
+
+func main() {
+	sum(1, 2, 3, 4, 5, 6, 7, 8)
+}
