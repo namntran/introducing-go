28356179dd5d9ec81a59ef4c95caf97c4dfe5587
namntran <namtran78@gmail.com>
2018-09-12 07:48:38 +1000

    add lesson on Contains function that searches for a smaller string in a bigger string

---
+++ ch8/ex1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// search for a smaller string in a bigger string
+func main() {
+	// Contain(s, substr string) bool
+	fmt.Println(strings.Contains("test", "st")) // Contains function
+	// => true
+}
