97a1964bc0130f41868bdd96336d269e2eb23b0d
namntran <namtran78@gmail.com>
2018-09-13 08:13:21 +1000

    add lesson - HasSuffix function to determine if a bigger string ends with a smaller string

---
+++ ch8/ex4/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() { // Use the HasSuffix function to determine if a bigger string ends with a smaller string
+	// func HasSufffix(s, suffix string) bool
+	fmt.Println(strings.HasSuffix("test", "st"))
+	// => true
+
+}
