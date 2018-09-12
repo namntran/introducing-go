25679fa3f10fde02692b1c1ec137bab5206e329b
namntran <namtran78@gmail.com>
2018-09-13 08:23:29 +1000

    add lesson - use Index function to find the position of a smaller string in a bigger string

---
+++ ch8/ex5/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() { // use the Index function to find the position of a smaller string in a bigger string
+	// func Index(s, sep string) int
+	fmt.Println(strings.Index("test", "e"))
+	// => 1
+}
