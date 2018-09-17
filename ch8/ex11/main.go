6213b20e4772a45ab93bbbd5925e94d89039677a
namntran <namtran78@gmail.com>
2018-09-18 08:41:01 +1000

    add lesson - convert a string to all uppercase letters using the ToUpper function

---
+++ ch8/ex11/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//convert a string to all uppercase letters
+func main() {
+	//func ToUpper(s string) string
+	fmt.Println(strings.ToUpper("test"))
+	// => TEST
+}
