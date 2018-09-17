582eca885250df2e239e0f452f4b3daa529019f8
namntran <namtran78@gmail.com>
2018-09-18 08:36:53 +1000

    add lesson - convert a string to all lower case letters using ToLower function

---
+++ ch8/ex10/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// convert a string to all lower case letters
+func main() {
+	// func ToLower(s string) string
+	fmt.Println(strings.ToLower("TEST"))
+	// => test
+
+}
