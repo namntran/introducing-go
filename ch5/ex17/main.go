ac84c2b1c068e7bc402883e5b598847b77cf4b05
namntran <namtran78@gmail.com>
2018-10-09 07:47:16 +1000

    add lesson examples

---
+++ ch5/ex17/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	conceptsInt() // calls func concepts
+	conceptsString()
+}
+
+func conceptsInt() {
+	firstName := 12
+	fmt.Println(firstName)
+
+}
+
+func conceptsString() {
+	lastName := 12
+	fmt.Println(lastName)
+}
